89436cff340c2be71743193e2c2da2ce922732d4
Dasha Buka <dvbuka@ucdavis.edu>
2024-04-12 11:23:41 -0700

    Added responsive Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,18 +4,19 @@ import Navbar from 'react-bootstrap/Navbar';
 import logo from '../images/nobg.png';
 function MyNavbar() {
     return (
-        <>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="/">
-                        <img
-                            alt=""
-                            src={logo}
-                            height="30"
-                            className="d-inline-block align-center"
-                        />{' '}
-                        Davis HvZ
-                    </Navbar.Brand>
+        <Navbar expand="lg" className="bg-body-tertiary" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="/">
+                    <img
+                        alt=""
+                        src={logo}
+                        height="30"
+                        className="d-inline-block align-center"
+                    />{' '}
+                    Davis HvZ
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="/about">What is HvZ?</Nav.Link>
                         <Nav.Link href="/participate">Participate</Nav.Link>
@@ -23,13 +24,10 @@ function MyNavbar() {
                         <Nav.Link href="/report">Report an Infection</Nav.Link>
                         <Nav.Link href="/account">Account</Nav.Link>
                         <Nav.Link href="/dnc" align="right">Davis Nerf Club</Nav.Link>
-
                     </Nav>
-                    <Nav className="justify-content-end">
-                    </Nav>
-                </Container>
-            </Navbar>
-        </>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
